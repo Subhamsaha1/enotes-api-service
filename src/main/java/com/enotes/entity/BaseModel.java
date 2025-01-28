624539a1530a9af3e0973918d5270b137c01445b
Subhamsaha1 <sahasubham48@gmail.com>
2025-01-29 01:00:36 +0530

    auditing implementation

--- src/main/java/com/enotes/entity/BaseModel.java
+++ src/main/java/com/enotes/entity/BaseModel.java
@@ -2,6 +2,12 @@ package com.enotes.entity;
 
 import java.util.Date;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,18 +19,22 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @MappedSuperclass
-public class BaseModel {
+public abstract class BaseModel {
 	
-	private Boolean isActive;
-
-	private Boolean isDeleted;
-
+	@CreatedBy
+	@Column(updatable = false)
 	private Integer createdBy;
 
+	@CreatedDate
+	@Column(updatable = false)
 	private Date createdOn;
 
+	@LastModifiedBy
+	@Column(insertable = false)
 	private Integer updatedBy;
 
+	@LastModifiedDate
+	@Column(insertable = false)
 	private Date updatedOn;
 
 }
