e51691b0246334dda4177dd03a26a83c44bf5298
Subhamsaha1 <sahasubham48@gmail.com>
2025-01-31 01:25:36 +0530

    generic-response logic implementation

--- src/main/java/com/enotes/controller/CategoryController.java
+++ src/main/java/com/enotes/controller/CategoryController.java
@@ -19,6 +19,7 @@ import com.enotes.dto.CategoryDto;
 import com.enotes.dto.CategoryResponse;
 import com.enotes.exception.ResourceNotFoundException;
 import com.enotes.service.CategoryService;
+import com.enotes.util.CommonUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -34,23 +35,24 @@ public class CategoryController {
 	public ResponseEntity<?> saveCategory(@RequestBody CategoryDto categoryDto){
 		Boolean saveCategory = categoryService.saveCategory(categoryDto);
 		if(saveCategory) {
-			return new ResponseEntity<>("saved successfully",HttpStatus.CREATED);
+			 return CommonUtil.createBuildResponseMessage("saved success", HttpStatus.CREATED);
+//			return new ResponseEntity<>("saved successfully",HttpStatus.CREATED);
 		}
 		else {
-			return new ResponseEntity<>("not saved",HttpStatus.INTERNAL_SERVER_ERROR);
+			return CommonUtil.createErrorResponseMessage("Category not saved", HttpStatus.INTERNAL_SERVER_ERROR);
+//			return new ResponseEntity<>("not saved",HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@GetMapping("/")
 	public ResponseEntity<?> getAllCategory(){
-//		String nm = null;
-//		nm.toUpperCase();
 		List<CategoryDto> allCategory = categoryService.getAllCategory();
 		if(CollectionUtils.isEmpty(allCategory)) {
 			return ResponseEntity.noContent().build();
 		}
 		else {
-			return new ResponseEntity<>(allCategory, HttpStatus.OK);
+			return CommonUtil.createBuildResponse(allCategory, HttpStatus.OK);
+//			return new ResponseEntity<>(allCategory, HttpStatus.OK);
 		}
 	}
 	
@@ -61,7 +63,8 @@ public class CategoryController {
 			return ResponseEntity.noContent().build();
 		}
 		else {
-			return new ResponseEntity<>(allCategory, HttpStatus.OK);
+			return CommonUtil.createBuildResponse(allCategory, HttpStatus.OK);
+//			return new ResponseEntity<>(allCategory, HttpStatus.OK);
 		}
 	}
 	
@@ -69,20 +72,22 @@ public class CategoryController {
 	public ResponseEntity<?> getCategoryDetailsById(@PathVariable Integer id) throws Exception{
 		CategoryDto categoryDto = categoryService.getCategoryById(id);
 		if(ObjectUtils.isEmpty(categoryDto)) {
-			return new ResponseEntity<>("Internal Server Error", HttpStatus.NOT_FOUND);
+			return CommonUtil.createErrorResponseMessage("Internal Server Error", HttpStatus.NOT_FOUND);
+//			return new ResponseEntity<>("Internal Server Error", HttpStatus.NOT_FOUND);
 		}
-		return new ResponseEntity<>(categoryDto, HttpStatus.OK);
-	
-		
+		return CommonUtil.createBuildResponse(categoryDto, HttpStatus.OK);
+//		return new ResponseEntity<>(categoryDto, HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deleteCategoryById(@PathVariable Integer id){
 		Boolean deleted = categoryService.deleteCategoryById(id);
 		if(deleted) {
-			return new ResponseEntity<>("Category deleted successfully", HttpStatus.OK);
+			return CommonUtil.createBuildResponse("Category deleted successfully", HttpStatus.OK);
+//			return new ResponseEntity<>("Category deleted successfully", HttpStatus.OK);
 		}
-		return new ResponseEntity<>("Category not deleted", HttpStatus.INTERNAL_SERVER_ERROR);
+		return CommonUtil.createErrorResponseMessage("Category not deleted", HttpStatus.INTERNAL_SERVER_ERROR);
+//		return new ResponseEntity<>("Category not deleted", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
--- src/main/java/com/enotes/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enotes/exception/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.enotes.util.CommonUtil;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -14,35 +16,43 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<?> handleException(Exception e){
-		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		log.error("GlobalExceptionHandler :: handleException ::", e.getMessage());
+//		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		return CommonUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	@ExceptionHandler(NullPointerException.class)
 	public ResponseEntity<?> handleNullPointerException(Exception e){
-		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		log.error("GlobalExceptionHandler :: handleNullPointerException ::", e.getMessage());
+//		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		return CommonUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<?> handleResourceNotFoundException(Exception e){
 		log.error("GlobalExceptionHandler :: handleResourceNotFoundException ::", e.getMessage());
-		return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+//		return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+		return CommonUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.NOT_FOUND);
 	}
 	
 	@ExceptionHandler(ValidationException.class)
 	public ResponseEntity<?> validationException(ValidationException e){
 		log.error("GlobalExceptionHandler :: validationException ::", e.getMessage());
-		return new ResponseEntity<>(e.getErrors(), HttpStatus.BAD_REQUEST);
+//		return new ResponseEntity<>(e.getErrors(), HttpStatus.BAD_REQUEST);
+		return CommonUtil.createErrorResponse(e.getErrors(), HttpStatus.BAD_REQUEST);
 	}
 	
 	@ExceptionHandler(ExistDataException.class)
 	public ResponseEntity<?> handleExistDataException(ExistDataException e){
 		log.error("GlobalExceptionHandler :: existDataException ::", e.getMessage());
-		return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+//		return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+		return CommonUtil.createBuildResponseMessage(null, null);
 	}
 	
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
 		log.error("GlobalExceptionHandler :: httpMessageNotReadableException ::", e.getMessage());
-		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		return CommonUtil.createBuildResponseMessage(null, null);
 	}
 }
