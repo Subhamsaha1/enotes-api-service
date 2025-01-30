08a1bebc808d8472e465b0b48a184358d612e0d8
Subhamsaha1 <sahasubham48@gmail.com>
2025-01-30 21:09:16 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/com/enotes/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enotes/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.enotes.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -37,5 +38,11 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<?> handleExistDataException(ExistDataException e){
 		log.error("GlobalExceptionHandler :: existDataException ::", e.getMessage());
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+	}
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
+		log.error("GlobalExceptionHandler :: httpMessageNotReadableException ::", e.getMessage());
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 }
